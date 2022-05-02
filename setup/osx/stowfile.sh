c4ad1757b7ccb94c7118a3d39cc668c30a412c06
Alex Chaichan <a.chaichan@icloud.com>
2022-05-02 18:23:11 +0200

    update

--- setup/osx/stowfile.sh
+++ setup/osx/stowfile.sh
@@ -23,11 +23,11 @@ ln -sf ~/.dotfiles/moc/.moc ~/
 ln -sf ~/.dotfiles/jupyter/.jupyter ~/
 ln -sf ~/.dotfiles/zathura ~/.config
 ln -sf ~/.dotfiles/htop/ ~/config
-ln -sf ~/.dotfiles/ranger/ranger ~/.config
+ln -sf ~/.dotfiles/ranger ~/.config
 ln -sf ~/.dotfiles/mpv ~/.config
 ln -sf ~/.dotfiles/neofetch ~/.config
-ln -sf ~/.dotfiles/khard/khard ~/.config
-ln -sf ~/.dotfiles/khal/khal ~/.config
+ln -sf ~/.dotfiles/khard ~/.config
+ln -sf ~/.dotfiles/khal ~/.config
 ln -sf ~/.dotfiles/git/.gitconfig ~/
 ln -sf ~/.dotfiles/git/.gitignore_global ~/ && git config --global core.excludesfile ~/.gitignore_global
 ln -sf ~/.dotfiles/spotify/spotifyd ~/.config
