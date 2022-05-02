2bb9f3518b5410121b926310337b89a177dd30ef
Alex Chaichan <a.chaichan@icloud.com>
2022-05-02 15:52:59 +0200

    update

--- setup/osx/pip.sh
+++ setup/osx/pip.sh
@@ -1,7 +1,13 @@
 # pip
 sudo easy_install pip
 sudo easy_install pip3
+
 pip3 install PyQt5
+pip install PyQtWebEngine
+pip3 install qutebrowser
+pip3 install asciidoc
+pip3 install adblock
+pip install tldextract
 
 # jupyter
 pip install JLDracula
--- setup/osx/repositories.sh
+++ setup/osx/repositories.sh
@@ -1,20 +1,9 @@
 # qutebrowser virtual einvironment
-
-pip install PyQtWebEngine
-pip3 install qutebrowser
-pip3 install asciidoc
-pip3 install adblock
-pip install tldextract
-
 git clone https://github.com/qutebrowser/qutebrowser.git
-
 cd qutebrowser
-
 python3 scripts/mkvenv.py
-
 # password manager script for pass x qutebrowser
 cp -r ~/.dotfiles/qutebrowser/.qutebrowser/userscripts ~/Library/Application\ Support/qutebrowser
-
 
 # pandoc
 git clone https://github.com/pandoc/lua-filters.git ~/.local/share/pandoc/filters
