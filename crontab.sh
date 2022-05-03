4e10426ee5fbefdcc2192d8f120131e5b3638c84
Alex Chaichan <a.chaichan@icloud.com>
2022-05-03 13:19:58 +0200

    update

--- crontab.sh
+++ crontab.sh
@@ -2,5 +2,3 @@
 
 # sync every 5 mins  mail, contacts and calendars
 (crontab -l; echo '*/5 * * * * /usr/local/bin/mbsync icloud && /usr/local/bin/vdirsyncer discover my_contacts && /usr/local/bin/vdirsyncer discover my_calendar  && /usr/local/bin/vdirsyncer sync >/dev/null 2>&1') | sort -u | crontab -
-
-
