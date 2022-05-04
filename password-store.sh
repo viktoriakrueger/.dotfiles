13b35cd50cbb2ba4b1a458ba784d53e06ba7666f
Aleksej Chaichan <a.chaichan@icloud.com>
2022-05-04 06:18:31 +0200

    update

--- password-store.sh
+++ password-store.sh
@@ -7,6 +7,6 @@ echo | pass spotify-tui > ~/.config/spotify-tui/client.yml
 
 if [[ "$os" == "osx" ]]; then
 	echo | pass spotifyd > ~/.cache/spotifyd/credentials.json
-elfi [[ "$os" == "linux" ]]; then
+elif [[ "$os" == "linux" ]]; then
 	echo | pass spotifyd > ~/.config/spotifyd/credentials.json
 fi
