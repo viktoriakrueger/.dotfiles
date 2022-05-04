fca5112ce773056195f27578136dcb0eb9980ff1
Aleksej Chaichan <a.chaichan@icloud.com>
2022-05-04 06:57:02 +0200

    update

--- R/R.sh
+++ R/R.sh
@@ -5,4 +5,4 @@ ln -sf ~/.dotfiles/rstudio/rstudio-prefs.json ~/.config/rstudio
 ln -sf ~/.dotfiles/nvim/snips/r.snippets ~/.config/rstudio/snippets
 
 # install R packages
-Rscript ~/.dotfiles/setup/osx/packages.R
+Rscript ~/.dotfiles/R/packages.R
--- osx/wgets.sh
+++ osx/wgets.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-# # qutebrowser
-# wget https://github.com/qutebrowser/qutebrowser/releases/download/v2.4.0/qutebrowser-2.4.0.dmg
-# hdiutil attach qutebrowser.dmg
-# cp -R /Volumes/qutebrowser.app /Applications
-
+# qutebrowser
+wget https://github.com/qutebrowser/qutebrowser/releases/download/v2.4.0/qutebrowser-2.4.0.dmg
+hdiutil attach qutebrowser-2.4.0.dmg 
+cp -R /Volumes/qutebrowser/qutebrowser.app /Applications
 
 # amphetamine
 wget https://github.com/x74353/Amphetamine-Enhancer/raw/master/Releases/Current/Amphetamine%20Enhancer.dmg
