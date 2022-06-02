f9cdd50a3293fd85fd92424dde8fb76d5fc4ea81
alexchaichan <a.chaichan@icloud.com>
2022-06-02 15:50:54 +0200

    update

--- R/package_manager.R
+++ R/package_manager.R
@@ -1,5 +1,6 @@
 #!/usr/bin/env R
 
+# source package-list
 source(paste0(Sys.getenv('HOME'),'/.dotfiles/R/packages.R'))
 
 installed_packages <- packages %in% rownames(installed.packages())
@@ -8,15 +9,8 @@ if (any(installed_packages == FALSE)) {
   install.packages(packages[!installed_packages], repos = "https://cloud.r-project.org")
 }
 
-# prefix <- c("/")
-# remotes <- c("")
-#
-# installed_remotes <- remotes %in% rownames(installed.packages())
-#
-# if (any(installed_remotes == FALSE)) {
-# remotes::install_github(paste0(prefix,remotes[!installed_remotes]))
-# }
-#
-# invisible(lapply(remotes, library, character.only = TRUE))
-#
-#
+installed_remotes <- strsplit(github,split='/', fixed=TRUE)[[1]] %in% rownames(installed.packages())
+
+if (any(installed_remotes == FALSE)) {
+remotes::install_github(paste0(prefix,remotes[!installed_remotes]))
+}
