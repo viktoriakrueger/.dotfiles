23b8cb2c45d29aae69df195d8cd17b84c4befd52
alexchaichan <a.chaichan@icloud.com>
2022-06-04 16:47:04 +0200

    update

--- tmux/plugins.sh
+++ tmux/plugins.sh
@@ -4,6 +4,7 @@ tmux_plugins=(
   'tmux-plugins/tmux-battery'
   'tmux-plugins/tmux-copycat'
   'tmux-plugins/tmux-cpu'
+  'tmux-plugins/tmux-sidebar'
   'tmux-plugins/tmux-maildir-counter'
   'tmux-plugins/tmux-online-status'
   'tmux-plugins/tmux-sensible'
