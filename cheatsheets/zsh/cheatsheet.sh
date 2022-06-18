9fda150405b7f165ca0df2a8b99d68b76baaf180
alexchaichan <a.chaichan@icloud.com>
2022-06-18 18:47:24 +0200

    update

--- cheatsheets/zsh/cheatsheet.sh
+++ cheatsheets/zsh/cheatsheet.sh
@@ -3,3 +3,8 @@ cd in/to/dir/ ; for f in *.txt; do (cat "${f}"; echo) >> finalfile.txt; done; cd
 
 # unite / merge pdfs * means all files in this dir
 pdfunite ~/path/to/directorie/* ~/new_file.pdf
+
+# zip
+zip -r FILENAME.zip /PATH/TO/DIR/*
+
+# unzip
