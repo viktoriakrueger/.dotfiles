59180b4db9c11dc4382b6b4589929632530aa4b9
alexchaichan <a.chaichan@icloud.com>
2022-06-25 12:13:36 +0200

    update

--- crontab.sh
+++ crontab.sh
@@ -2,8 +2,8 @@
 
 # sync every 5 mins  mail, contacts and calendars
 if [[ "$os" == "osx" ]]; then
-  (crontab -l; echo '*/1 * * * * /usr/local/bin/mbsync icloud && /usr/local/bin/vdirsyncer discover my_contacts && /usr/local/bin/vdirsyncer discover my_calendar  && /usr/local/bin/vdirsyncer sync >/dev/null 2>&1') | sort -u | crontab -
+  (crontab -l; echo '*/1 * * * * /usr/local/bin/mbsync posteo && /usr/local/bin/vdirsyncer discover my_contacts && /usr/local/bin/vdirsyncer discover my_calendar  && /usr/local/bin/vdirsyncer sync >/dev/null 2>&1') | sort -u | crontab -
 elif [[ "$os" == "linux" ]]; then
-  (crontab -l; echo '*/1 * * * * /usr/bin/mbsync icloud && /usr/bin/vdirsyncer discover my_contacts && /usr/bin/vdirsyncer discover my_calendar  && /usr/bin/vdirsyncer sync >/dev/null 2>&1') | sort -u | crontab - &&
+  (crontab -l; echo '*/1 * * * * /usr/bin/mbsync posteo && /usr/bin/vdirsyncer discover my_contacts && /usr/bin/vdirsyncer discover my_calendar  && /usr/bin/vdirsyncer sync >/dev/null 2>&1') | sort -u | crontab - &&
   systemctl enable --now cronie.service
 fi
