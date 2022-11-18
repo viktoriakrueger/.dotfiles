d5bbc473a2fbe1985153c91c6caf2ea085e18da6
alexchaichan <a.chaichan@icloud.com>
2022-11-18 18:44:46 +0100

    update

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -1,5 +1,13 @@
 mkdir $HOME/.vscode; mkdir $HOME/.vscode/extensions; ln -sf $DOTFILES/vscode/.vscode/argv.json $HOME/.vscode/
 
+ln -sf $DOTFILES/vscode/settings.json $HOME/Library/Application\ Support/Code/User
+
+ln -sf $DOTFILES/snippets/ $HOME/Library/Application\ Support/Code/User
+
+ln -sf $DOTFILES/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User
+
+ln -sf $DOTFILES/vscode/snippets $HOME/Library/Application\ Support/Code/User
+
 # dracula theme
 git clone https://github.com/dracula/visual-studio-code.git $HOME/.vscode/extensions/theme-dracula; cd $HOME/.vscode/extensions/theme-dracula; npm install; npm run build
 
