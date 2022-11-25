a423fedcc468958445129840496e498822200f26
alexchaichan <a.chaichan@icloud.com>
2022-11-25 18:41:22 +0100

    update

--- osx/duti.sh
+++ osx/duti.sh
@@ -54,3 +54,19 @@ duti -s com.vladbadea.csveditor .csv all
 # qutebrowser
 #duti -s org.qt-project.Qt.QtWebEngineCore .html all
 
+# vlc
+duti -s org.videolan.vlc .mp3 all
+duti -s org.videolan.vlc .mp4 all
+duti -s org.videolan.vlc .wma all
+duti -s org.videolan.vlc .ogg all
+duti -s org.videolan.vlc .m4a all
+duti -s org.videolan.vlc .wav all
+duti -s org.videolan.vlc .aac all
+duti -s org.videolan.vlc .aax all
+duti -s org.videolan.vlc .aiff all
+duti -s org.videolan.vlc .mpc all
+duti -s org.videolan.vlc .toc all
+duti -s org.videolan.vlc .webm all
+duti -s org.videolan.vlc .svi all
+duti -s org.videolan.vlc .avi all
+duti -s org.videolan.vlc .flv all
