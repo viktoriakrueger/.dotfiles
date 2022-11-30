2d77d92edb080d3b5fc4a0c29ab4f9d43827d3e5
alexchaichan <a.chaichan@icloud.com>
2022-12-01 00:13:23 +0100

    update

--- ruby/gems.sh
+++ ruby/gems.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
-geim install neovim
+# neoviom
+gem install neovim
+
+# colorls
 cd /usr/local/Cellar/ruby/3.1.2_1/bin && ./gem install colorls && ln -s /usr/local/lib/ruby/gems/3.1.0/bin/colorls /usr/local/bin/colorls
+
+#
+cd /usr/local/Cellar/ruby/3.1.2_1/bin && ./gem install bundler jekyll && ln -s /usr/local/lib/ruby/gems/3.1.0/bin/jekyll /usr/local/bin/jekyll
+cd /usr/local/Cellar/ruby/3.1.2_1/bin && ./gem install jekyll && ln -s /usr/local/lib/ruby/gems/3.1.0/bin/jekyll /usr/local/bin/jekyll
+
+# jruby
+
+# tabula-extractor extract tables from pdf's
 jruby -S gem install tabula-extractor
+
