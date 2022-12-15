fecdcde892240b068afb2fe4705e975037ea3ea2
alexchaichan <a.chaichan@icloud.com>
2022-12-15 12:53:23 +0100

    update

---
+++ spotify/spotifyd/installer.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/Spotifyd/spotifyd.git
+
+cd $HOME/spotifyd
+
+cargo build --release --locked --no-default-features --features="portaudio_backend"
+cargo install --path . --locked --no-default-features --features="portaudio_backend"
